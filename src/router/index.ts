b73030216a8c61fbd136ccad69e9b74de0de4ed6
Igor Štumberger <hi@igorstumberger.com>
2022-10-12 22:44:21 +0200

    created heymetaview
    nothing added, just temp stuff

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/heymeta",
+      name: "heymeta",
+      // route level code-splitting
+      component: () => import("../views/HeymetaView.vue"),
+    },
   ],
 });
 
---
+++ src/views/HeymetaView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="heymeta">
+    <form action="" method="post">
+
+      <p class="website">Website URL:</p>
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+  
